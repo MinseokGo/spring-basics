1803d9d5f0bd2f980afffad16d391a70b8a44f46
MinseokGo <rhalstjr1999@naver.com>
2023-06-12 20:27:26 +0900

    chap08.6 update

--- src/main/java/chap08/config/AppCtx.java
+++ src/main/java/chap08/config/AppCtx.java
@@ -1,5 +1,32 @@
 package chap08.config;
 
-public class AppCtx {
+import chap08.spring.MemberDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.tomcat.jdbc.pool.DataSource;
 
+@Configuration
+public class AppCtx {
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+        ds.setUsername("spring5");
+        ds.setPassword("spring5");
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setMaxIdle(10);
+        ds.setTestWhileIdle(true);
+        ds.setMinEvictableIdleTimeMillis(60000 * 3);
+        ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
+
+        return ds;
+    }
+
+    @Bean
+    public MemberDao memberDao() {
+        return new MemberDao(dataSource());
+    }
 }
--- src/main/java/chap08/spring/MemberDao.java
+++ src/main/java/chap08/spring/MemberDao.java
@@ -1,12 +1,12 @@
 package chap08.spring;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Collection;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
 import java.util.List;
 
 public class MemberDao {
@@ -16,7 +16,39 @@ public class MemberDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public Collection<Member> selectAll() {
+    public void insert(final Member member) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement pstmt = con.prepareStatement(
+                    "insert into MEMBER (EMAIL, PASSWORD, NAME, REGDATE) " +
+                            "values (?, ?, ?, ?)",
+                    new String[] {"ID"});
+            pstmt.setString(1, member.getEmail());
+            pstmt.setString(2, member.getPassword());
+            pstmt.setString(3, member.getName());
+            pstmt.setTimestamp(4, Timestamp.valueOf(member.getRegisterDateTime()));
+
+            return pstmt;
+        }, keyHolder);
+        Number keyValue = keyHolder.getKey();
+        member.setId(keyValue.longValue());
+    }
+
+    public void update(Member member) {
+        jdbcTemplate.update(
+                "update MEMBER set NAME = ?, PASSWORD = ? where EMAIL = ?",
+                member.getName(), member.getPassword(), member.getEmail());
+    }
+
+    public int count() {
+        Integer count = jdbcTemplate.queryForObject(
+                "select count(*) from MEMBER", Integer.class);
+
+        return count;
+    }
+
+    public List<Member> selectAll() {
         List<Member> results = jdbcTemplate.query(
                 "select * from MEMBER",
                     new MemberRowMapper());
